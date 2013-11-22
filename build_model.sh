a6d16a70db519c5ecb424eadaba41c724ab8d852
Andrew Russell <deadhead.russell@gmail.com>
2013-11-21 20:21:08 -0500

    Added f0 exceptions. Has the default f0.params values for now.

--- build_model.sh
+++ build_model.sh
@@ -8,6 +8,27 @@ export SPTKDIR=/usr/local/SPTK
 ./bin/do_clustergen mcep
 ./bin/do_clustergen voicing
 ./bin/awb_do_clustergen f0_v_sptk
+
+cp etc/f0.params f0.params.tmp
+
+grep "NaeBonnieLaddie" etc/txt.done.data > tmp.data
+echo -e "F0MEAN=412\nF0STD=180\nF0MAX=700\nF0MIN=170" > etc/f0.params
+./bin/awb_do_clustergen f0_v_sptk tmp.data
+
+grep "TheBeggarMan" etc/txt.done.data > tmp.data
+echo -e "F0MEAN=412\nF0STD=180\nF0MAX=700\nF0MIN=170" > etc/f0.params
+./bin/awb_do_clustergen f0_v_sptk tmp.data
+
+grep "Edward" etc/txt.done.data > tmp.data
+echo -e "F0MEAN=412\nF0STD=180\nF0MAX=700\nF0MIN=170" > etc/f0.params
+./bin/awb_do_clustergen f0_v_sptk tmp.data
+
+grep "ABegginIWillGo" etc/txt.done.data > tmp.data
+echo -e "F0MEAN=412\nF0STD=180\nF0MAX=700\nF0MIN=170" > etc/f0.params
+./bin/awb_do_clustergen f0_v_sptk tmp.data
+
+cp f0.params.tmp etc/f0.params.tmp
+
 ./bin/do_clustergen combine_coeffs_v
 
 ./bin/do_clustergen generate_statenames
